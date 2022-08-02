6c9f3fa4492d6fba2cb1d2be4da24ca61c466841
joongwon <wnddnjs823@ajou.ac.kr>
2022-08-02 19:52:53 +0900

    첫 네트워크 통신때 어제 기준 날짜의 정보 초기화, 일간 관객수 기준 등수 표시

---
+++ SeSAC2LectureNetworkBasic/SeSAC2LectureNetworkBasic/BoxOfficeModel.swift
@@ -0,0 +1,17 @@
+//
+//  BoxOfficeModel.swift
+//  SeSAC2LectureNetworkBasic
+//
+//  Created by 이중원 on 2022/08/02.
+//
+
+import Foundation
+
+struct BoxOfficeModel {
+    
+    let rank: String
+    let movieTitle: String
+    let releaseDate: String
+    let todayCount: String
+    let totalCount: String
+}
--- SeSAC2LectureNetworkBasic/SeSAC2LectureNetworkBasic/SearchViewController.swift
+++ SeSAC2LectureNetworkBasic/SeSAC2LectureNetworkBasic/SearchViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+import Alamofire
+import SwiftyJSON
+
 /*
  Swift Protocol
  - Delegate
@@ -15,6 +18,12 @@ import UIKit
  1. 왼팔/오른팔
  2. 테이블 뷰 아울렛 연결
  3. 1 + 2
+ */
+
+/*
+ 
+ 각 json value ->
+ 
  */
 
 extension UIViewController {
@@ -27,10 +36,29 @@ extension UIViewController {
 class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
 
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var searchTableView: UITableView!
+    
+    let calendar = Calendar.current
+    let today = Date()
+    
+    //BoxOffice 배열
+    var list: [BoxOfficeModel] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let yesterday = calendar.date(byAdding: .day, value: -1, to: today)
+        
+        let yesterdayYearText = String(calendar.dateComponents([.year], from: yesterday!).year!)
+        let yesterdayMonth = calendar.dateComponents([.month], from: yesterday!).month!
+        let yesterdayMonthText: String = yesterdayMonth / 10 == 0 ? "0\(yesterdayMonth)" : "\(yesterdayMonth)"
+        let yesterdayDay = calendar.dateComponents([.day], from: yesterday!).day!
+        let yesterdayDayText: String = yesterdayDay / 10 == 0 ? "0\(yesterdayDay)" : "\(yesterdayDay)"
+        
+        let yesterdayText = "\(yesterdayYearText)\(yesterdayMonthText)\(yesterdayDayText)"
+        
+        print(yesterdayText)
         
         searchTableView.backgroundColor = .clear
 
@@ -40,20 +68,61 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         //테이블 뷰가 사용할 테이블 뷰 셀(XIB) 등록
         //XIB: Xml Interface Builder <= NIB
         searchTableView.register(UINib(nibName: ListTableViewCell.reuseIdentifier, bundle: nil), forCellReuseIdentifier: ListTableViewCell.reuseIdentifier)
+        
+        searchBar.delegate = self
+        requestBoxOffice(text: yesterdayText)
     }
     
-//    func configureView() {
-//        searchTableView.backgroundColor = .clear
-//        searchTableView.separatorColor = .clear
-//        searchTableView.rowHeight = 60
-//    }
-//
-//    func configureLabel() {
-//        <#code#>
-//    }
+    func configureView() {
+        searchTableView.backgroundColor = .clear
+        searchTableView.separatorColor = .clear
+        searchTableView.rowHeight = 60
+    }
+    
+    func configureLabel() {
+        
+    }
+
+    func requestBoxOffice(text: String) {
+        
+        list.removeAll()
+        
+        let url = "\(EndPoint.boxOfficeURL)key=\(APIKey.BOXOFFICE)&targetDt=\(text)"
+        
+        AF.request(url, method: .get).validate().responseJSON {response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                
+                //self.list.removeAll() => 통신에 성공했을 때 지울지?
+                                                
+                for movie in json["boxOfficeResult"]["dailyBoxOfficeList"].arrayValue {
+                    let rank = movie["rank"].stringValue
+                    let movieNm = movie["movieNm"].stringValue
+                    let openDt = movie["openDt"].stringValue
+                    let audiCnt = movie["audiCnt"].stringValue
+                    let audiAcc = movie["audiAcc"].stringValue
+                    
+                    let data = BoxOfficeModel(rank: rank, movieTitle: movieNm, releaseDate: openDt, todayCount: audiCnt, totalCount: audiAcc)
+                    
+                    self.list.append(data)
+                }
+                
+                //테이블뷰 갱신
+                self.searchTableView.reloadData()
+                
+                print(self.list)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -61,10 +130,18 @@ class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDa
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.reuseIdentifier, for: indexPath) as? ListTableViewCell else { return UITableViewCell() }
         
         cell.backgroundColor = .clear
-        cell.titleLabel.font = .boldSystemFont(ofSize: 22)
-        cell.titleLabel.text = "HELLO JOONGWON"
+        cell.titleLabel.font = .boldSystemFont(ofSize: 16)
+        cell.titleLabel.text = "\(list[indexPath.row].rank)등: '\(list[indexPath.row].movieTitle)'- 일간 \(list[indexPath.row].todayCount), 누적 \(list[indexPath.row].totalCount)명"
         
         return cell
     }
 
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        requestBoxOffice(text: searchBar.text!) //옵셔널 바인딩, 8글자, 숫자, 날짜로 변경 시 유효한 형태의 값인지 등
+    }
+    
 }
